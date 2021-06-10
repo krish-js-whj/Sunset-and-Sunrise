ec1efbb882b5b1f1276babfa138f8f49629b356b
krish-js-whj <77682371+krish-js-whj@users.noreply.github.com>
2021-06-10 13:57:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,12 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var backgroundImg;
-
-var bg,time=0,gameState="auto" ;
+var response,responseJSON,datetime,hour=0,hourS,minS,hourNmin;
+var bg,time=0,Hour=10,gameState="auto" ;
 
 function preload() {
     getBackgroundImg();
+  //  getbg()
 }
 function setup(){
     var canvas = createCanvas(1200,700);
@@ -24,53 +25,73 @@ function draw()
     }
     else 
     background(0)
+    console.log("BackgroundIsLoading")
      // add condition to check if any background image is there to add
-     if (gameState=="RT"){
-getBackgroundImg();
+       getBackgroundImg();
+   
+        if (time>2359){
+       time=0;
+    }if (Hour>23.59){
+        Hour=0;
      }
-    else{
-        time+=1;
-        getbg()
-    }Engine.update(engine);
+    Engine.update(engine);
     // write code to display time in correct format here
     fill(0)
     textSize(50)
-    text(time,50,100)
+
+   if(gameState=="RT")
+   {   if (time<1200)
+       text(hourS+":"+minS+" A.M.",50,100);
+        else 
+        text(hourS+":"+minS+" P.M.",50,100);
+    }
+    else {
+        Hour+=0.01;
+         getbg()
+      //  text(time,100,100)
+     // text((Math.round(Hour*10)/10),100,500)
+       text(Math.round(Hour),50,100);
+    //   text(bg,50,200)
+
+      
+    }
     textSize(25)
     fill(225)
     text("Toggle between real time view and auto view by pressing left arrow and right arrow respectively. ",50,650)
-   // console.log(time)
- 
-}
-async function getBackgroundImg(){
+    text("You might experience delay while switching",50,675)
+    // console.log(time)
+  // getbg()
 
-     response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-     responseJSON = await response.json();
-
-     datetime = responseJSON.datetime;
-     hour = datetime.slice(11,13);
-     min = datetime.slice(14,16);
-     hour= hour.toString();
-     min= min.toString();
-     hourNmin=hour.concat(min)
-    if (gameState=="auto"){
-     if (time>2359){
-       time=0;
-    }
-    }
-   else{
-    time=parseFloat(hourNmin)
-    }
 }
 function keyPressed(){
     if (keyCode==RIGHT_ARROW){
         gameState="auto";
+        getbg()
     }if (keyCode==LEFT_ARROW){
         gameState="RT"
+        getBackgroundImg();
     }
 }
-function getbg(){
-if(time>=0400 && time<=0600){
+async function getBackgroundImg(){
+
+     
+    
+    if (gameState=="RT"){ 
+        response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+     responseJSON = await response.json();
+     datetime = responseJSON.datetime;
+     hour = datetime.slice(11,13);
+     min = datetime.slice(14,16);
+     hourS= hour.toString();
+     minS= min.toString();
+     hourNmin=hourS.concat(minS);
+     time=parseFloat(hourNmin)
+     console.log(time)
+    }
+    if (gameState=="auto"){
+    }
+     if(time>=0400 &&time<=0600)
+     {
         bg = "sunrise1.png";
     }else if(time>=0601 && time<=0800){
         bg = "sunrise2.png";
@@ -83,7 +104,7 @@ if(time>=0400 && time<=0600){
     }else if(time>=1301 && time<=1430){
         bg = "sunrise5.png";
     }else if(time>=1431 && time<=1600){
-        bg = "sunrise6.png";
+        bg = "sunrise5.png";
     }else if(time>=1601 && time<=1700){
         bg = "sunset7.png";
     }else if(time>=1701 && time<=1800){
@@ -100,5 +121,40 @@ if(time>=0400 && time<=0600){
         bg = "sunset11.png";
     }
     backgroundImg = loadImage(bg);
-
+   
 }
+
+function getbg()
+{if((Math.round(Hour*10)/10)>=4.01 &&(Math.round(Hour*10)/10)<=6)
+     {
+        bg = "sunrise1.png";
+    } if((Math.round(Hour*10)/10)>=6.01 && (Math.round(Hour*10)/10)<=8.00){
+        bg = "sunrise2.png";
+    } if((Math.round(Hour*10)/10)>=8.01 && (Math.round(Hour*10)/10)<=9.00){
+        bg = "sunrise3.png";
+    } if((Math.round(Hour*10)/10)>=9.01 && (Math.round(Hour*10)/10)<=11.51){
+        bg = "sunrise4.png";
+    } if((Math.round(Hour*10)/10)>=11.51 && (Math.round(Hour*10)/10)<=13.00){
+        bg = "sunrise6.png";
+    } if((Math.round(Hour*10)/10)>=13.01 && (Math.round(Hour*10)/10)<=14.51){
+        bg = "sunrise5.png";
+    } if((Math.round(Hour*10)/10)>=14.51 && (Math.round(Hour*10)/10)<=16.00){
+        bg = "sunrise6.png";
+    } if((Math.round(Hour*10)/10)>=16.01 && (Math.round(Hour*10)/10)<=17.00){
+        bg = "sunset7.png";
+    } if((Math.round(Hour*10)/10)>=17.01 && (Math.round(Hour*10)/10)<=18.00){                                                     
+        bg = "sunset8.png";
+    } if((Math.round(Hour*10)/10)>=18.01 && (Math.round(Hour*10)/10)<=18.51){
+        bg = "sunset9.png";
+    } if((Math.round(Hour*10)/10)>=18.51 && (Math.round(Hour*10)/10)<=19.00){
+        bg = "sunset10.png";
+    } if((Math.round(Hour*10)/10)>=19.01 && (Math.round(Hour*10)/10)<=20.51){
+        bg = "sunset11.png";
+    } if((Math.round(Hour*10)/10)>=20.51 && (Math.round(Hour*10)/10)<=23.99){
+        bg = "sunset12.png";
+    } if((Math.round(Hour*10)/10)>=0.00 && (Math.round(Hour*10)/10)<=3.00){
+        bg = "sunset12.png";
+    } if((Math.round(Hour*10)/10)>=3.01 && (Math.round(Hour*10)/10)<=4.00){
+        bg = "sunset11.png";
+    }
+    backgroundImg = loadImage(bg);}
